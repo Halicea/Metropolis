5e28bcfa188d90f29fa6520623f514d4d54be86c
Costa Halicea <costa@halicea.com>
2011-04-18 19:59:08 +0200

    menu order changed

--- src/menu.py
+++ src/menu.py
@@ -1,9 +1,9 @@
 menu = (
-('/', 'Home', False),
-('/Metropolis/Shop', 'Shops', False),
 ('/Metropolis/Product/Search/', 'Search', False),
+('/Metropolis/Shop', 'Shops', False),
 ('/Metropolis/Company', 'Companies', False),
 ('/Metropolis/Product', 'Products', False),
 ('/Metropolis/Promotion', 'Promotions', False),
 ('/Metropolis/Grouper', 'Grouper', False),
+('/', 'Development Links', False),
 )
